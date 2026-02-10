4cb0500198242cc77b3e94ec6c0415e5109e324d
hendisantika <hendisantika@yahoo.co.id>
2026-02-11 06:33:44 +0700

    feat: add Order Repository 🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/opentelemetry/repository/OrderRepository.java
@@ -0,0 +1,18 @@
+package id.my.hendisantika.opentelemetry.repository;
+
+import id.my.hendisantika.opentelemetry.entity.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : opentelemetry
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 11/02/26
+ * Time: 06.33
+ * To change this template use File | Settings | File Templates.
+ */
+public interface OrderRepository extends JpaRepository<Order, Long> {
+}
